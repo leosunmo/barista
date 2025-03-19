58fea64e214ab8a3ff368ad5103affc6a85a016f
leo sunmo <leo.sunmo@gmail.com>
2025-03-19 20:13:31 +0100

    Remove unused gsuite oauth config
    The Gmail integration was removed from sample bar as it was
    using soumya92's own OAuth credentials.

--- samples/sample-bar/sample-bar.go
+++ samples/sample-bar/sample-bar.go
@@ -226,16 +226,6 @@ func makeIconOutput(key string) *bar.Segment {
 	return outputs.Pango(spacer, pango.Icon(key), spacer)
 }
 
-var gsuiteOauthConfig = []byte(`{"installed": {
-	"client_id":"%%GOOGLE_CLIENT_ID%%",
-	"project_id":"i3-barista",
-	"auth_uri":"https://accounts.google.com/o/oauth2/auth",
-	"token_uri":"https://www.googleapis.com/oauth2/v3/token",
-	"auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
-	"client_secret":"%%GOOGLE_CLIENT_SECRET%%",
-	"redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]
-}}`)
-
 func threshold(out *bar.Segment, urgent bool, color ...bool) *bar.Segment {
 	if urgent {
 		return out.Urgent(true)
